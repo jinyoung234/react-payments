0119056c93deb0006c29e6f7ad41a692bf7bad59
손진영 <sm099@naver.com>
2024-04-18 16:32:00 +0900

    build: vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dist/',
   resolve: {
     alias: [
       { find: '@src', replacement: resolve(__dirname, 'src') },
